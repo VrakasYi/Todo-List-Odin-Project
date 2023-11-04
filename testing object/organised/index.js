2fedb4c6e64d3f8ffcb747a777d3a22fe3efff95
VrakasYi <vrakasyi@outlook.com>
2023-11-04 22:31:13 +0200

    to code: add task function

--- testing object/organised/index.js
+++ testing object/organised/index.js
@@ -23,10 +23,12 @@ const projectDataModule = (() => {
 
 //projectListModule.js
 const projectListModule = (() => {
+    //reference the list of to do container
     const projectListDIV = document.getElementById('projectList');
     const newListButton = document.getElementById('newList');
     const popup = document.getElementById('popup');
     const form = document.getElementById('newListForm');
+    const listBox = document.getElementById('listBox');
 
     newListButton.addEventListener('click', () => {
         popup.classList.remove('inactive');
@@ -45,29 +47,39 @@ const projectListModule = (() => {
         popup.classList.add('inactive');
     });
 
+    projectListDIV.addEventListener('click', (event) => {
+        if (event.target.tagName === 'BUTTON') {
+            const projectId = event.target.id;
+            listBox.innerHTML = '';
+    
+            const listEntry = document.createElement('div');
+            listEntry.textContent = projectId;
+    
+            const addNewTaskButton = document.createElement('button');
+            addNewTaskButton.textContent = 'Add new task';
+            addNewTaskButton.id = 'newTaskButton'
+    
+            listEntry.appendChild(addNewTaskButton);
+            listBox.appendChild(listEntry);
+    
+            const findObject = projectDataModule.getProject(projectId);
+        };
+    });
 })();
 
 //ListBoxModule.js
 const ListBoxModule = (() => {
     //reference the list of to do container
     const listBox = document.getElementById('listBox');
-    const projectListDIV = document.getElementById('projectList');
+    //reference the new task pop up form
+    const newTaskPop = document.getElementById('newTaskPop');
 
-    projectListDIV.addEventListener('click', (event) => {
-        if (event.target.tagName === 'BUTTON') {
-            const projectId = event.target.id;
-            listBox.innerHTML = '';
-
-            const listEntry = document.createElement('div');
-            listEntry.textContent = projectId;
-
-            const addNewTaskButton = document.createElement('button');
-            addNewTaskButton.textContent = 'Add new task';
-
-            listEntry.appendChild(addNewTaskButton);
-            listBox.appendChild(listEntry);
-
-            const findObject = projectDataModule.getProject(projectId);
-        }
+    listBox.addEventListener('click', (event) => {
+        if (event.target.id === 'newTaskButton') {
+            console.log('you need a pop up');
+            newTaskPop.classList.remove('inactive')
+            newTaskPop.classList.add('active')
+        };
     });
+
 })();
